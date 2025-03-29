982e346de58d11f8755787a902915ea5a4aa7f5c
RickCBA <rick@closedby.ai>
2025-03-29 14:01:17 +0100

    Update widget-formJS.js

--- widget-formJS.js
+++ widget-formJS.js
@@ -39,21 +39,21 @@ setTimeout(() => {
     cannedMsgDiv.className = 'chat-message chat-message-from-bot my-canned-bot-message';
 
     cannedMsgDiv.innerHTML = `
-      <div style="position: relative;">
-        <p style="margin-bottom:14px; font-style: italic;">
-          *We may store personal data and use it to contact you to support your dental needs.
-          See <a href="https://www.sharplesdental.co.uk/privacy-policy" target="_blank" style="color: inherit; text-decoration: underline;">Privacy Policy</a>*
-        </p>
-        <div class="canned-messages-grid">
-          <button data-message="Book an Appointment" data-send="Hello, I’d like to book an appointment. Can you provide me with my options?">Book an Appointment</button>
-          <button data-message="Emergency Appointment" data-send="I’m experiencing a dental emergency and need to be seen soon. Can you tell me my options?">Emergency Appointment</button>
-          <button data-message="Dental Check-up & Cleaning" data-send="Hi, I’d like a routine dental check-up and cleaning. Could you provide me with the options?">Dental Check-up & Cleaning</button>
-          <button data-message="Straighten My Teeth" data-send="I’m interested in straightening my teeth. Could you share the options?">Straighten My Teeth</button>
-          <button data-message="Replace Missing Teeth" data-send="Hi, I need to replace a missing tooth (or teeth). Can you tell me about implants or other solutions?">Replace Missing Teeth</button>
-          <button data-message="Teeth Whitening & Cosmetic Treatments" data-send="Hello, I’m looking for teeth whitening or cosmetic dentistry options. What treatments do you offer?">Teeth Whitening & Cosmetic Treatments</button>
-          <button data-message="I want to ask a question" data-send="I have a question, can you help me to answer it?">I want to ask a question</button>
-        </div>
-      </div>
+            <div style="position: relative;">
+              <p style="margin-bottom:14px; font-style: italic;">
+                *We may store personal data and use it to contact you to support your dental needs.
+                See <a href="https://www.sharplesdental.co.uk/privacy-policy" target="_blank" style="color: inherit; text-decoration: underline;">Privacy Policy</a>*
+              </p>
+              <div class="canned-messages-grid">
+                <button data-message="Book an Appointment" data-send="Hello, I’d like to book an appointment.">Book an Appointment</button>
+                <button data-message="Emergency Appointment" data-send="I’m experiencing a dental emergency and need to be seen soon. Can someone contact me urgently">Emergency Appointment</button>
+                <button data-message="Dental Check-up & Cleaning" data-send="Hi, I’d like a routine dental check-up and cleaning.">Dental Check-up & Cleaning</button>
+                <button data-message="Straighten My Teeth" data-send="I’m interested in straighter teeth and a better smile.">Straighten My Teeth</button>
+                <button data-message="Replace Missing Teeth" data-send="Hi, I need to replace a missing tooth (or teeth).">Replace Missing Teeth</button>
+                <button data-message="Teeth Whitening & Cosmetic Treatments" data-send="Hello, I’m looking for teeth whitening or cosmetic dentistry options. Let's discuss my goals">Teeth Whitening & Cosmetic Treatments</button>
+                <button data-message="I want to ask a question" data-send="I have a question, can you help me to answer it?">I want to ask a question</button>
+              </div>
+            </div>
     `;
 
     chatMessagesList.insertBefore(cannedMsgDiv, chatMessagesList.firstChild);
